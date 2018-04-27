b04b4ed36c89b8ca19602251d13e0c0a5602b1f9
Andrei Motinga <andrew.motinga@gmail.com>
2018-04-27 17:37:31 -0400

    add button to create listing

--- client/src/components/App/Nav/Nav.js
+++ client/src/components/App/Nav/Nav.js
@@ -2,34 +2,46 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { withStyles } from "material-ui/styles";
+import { connect } from "react-redux";
+import { openDialog } from "actions";
 import AppBar from "material-ui/AppBar";
 import Toolbar from "material-ui/Toolbar";
+import Button from "material-ui/Button";
+import history from "config/history";
 
 import Logo from "./Logo";
 import AuthDropdown from "./AuthDropdown";
 
-class Nav extends React.Component {
-  render() {
-    const { classes } = this.props;
+const Nav = ({ classes, currentUser, openAuthDialog }) => {
+  const sendToEdit = () => {
+    if (currentUser) {
+      history.push("/listings/new/edit");
+    } else {
+      openAuthDialog();
+    }
+  };
 
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <Link to="/">
-              <Logo />
-            </Link>
-            <div className={classes.spacer} />
-            <AuthDropdown />
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Button onClick={sendToEdit}>Создать</Button>
+
+          <Link to="/">
+            <Logo />
+          </Link>
+          <div className={classes.spacer} />
+          <AuthDropdown />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
 Nav.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  currentUser: PropTypes.object,
+  openAuthDialog: PropTypes.func.isRequired
 };
 
 const styles = {
@@ -38,4 +50,16 @@ const styles = {
   }
 };
 
-export default withStyles(styles)(Nav);
+const styledNav = withStyles(styles)(Nav);
+
+const mapStateToProps = state => ({
+  currentUser: state.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  openAuthDialog: () => {
+    dispatch(openDialog("AuthDialog"));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(styledNav);
