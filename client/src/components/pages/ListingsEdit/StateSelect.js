d1668d7bacc196e2719a2d2a38f945e81149be36
Andrei Motinga <andrew.motinga@gmail.com>
2018-04-28 12:28:30 -0400

    extract suggestions

--- client/src/components/pages/ListingsEdit/StateSelect.js
+++ client/src/components/pages/ListingsEdit/StateSelect.js
@@ -12,46 +12,7 @@ import ClearIcon from "material-ui-icons/Clear";
 import Chip from "material-ui/Chip";
 import Select from "react-select";
 import "react-select/dist/react-select.css";
-
-const suggestions = [
-  { label: "Afghanistan" },
-  { label: "Aland Islands" },
-  { label: "Albania" },
-  { label: "Algeria" },
-  { label: "American Samoa" },
-  { label: "Andorra" },
-  { label: "Angola" },
-  { label: "Anguilla" },
-  { label: "Antarctica" },
-  { label: "Antigua and Barbuda" },
-  { label: "Argentina" },
-  { label: "Armenia" },
-  { label: "Aruba" },
-  { label: "Australia" },
-  { label: "Austria" },
-  { label: "Azerbaijan" },
-  { label: "Bahamas" },
-  { label: "Bahrain" },
-  { label: "Bangladesh" },
-  { label: "Barbados" },
-  { label: "Belarus" },
-  { label: "Belgium" },
-  { label: "Belize" },
-  { label: "Benin" },
-  { label: "Bermuda" },
-  { label: "Bhutan" },
-  { label: "Bolivia, Plurinational State of" },
-  { label: "Bonaire, Sint Eustatius and Saba" },
-  { label: "Bosnia and Herzegovina" },
-  { label: "Botswana" },
-  { label: "Bouvet Island" },
-  { label: "Brazil" },
-  { label: "British Indian Ocean Territory" },
-  { label: "Brunei Darussalam" }
-].map(suggestion => ({
-  value: suggestion.label,
-  label: suggestion.label
-}));
+import suggestions from "./states";
 
 class Option extends React.Component {
   handleClick = event => {
