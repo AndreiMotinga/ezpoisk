c92c85be3d4ebbde97c8062c369ca35678ef9437
Andrei Motinga <andrew.motinga@gmail.com>
2018-04-28 12:55:51 -0400

    break down in smaller

---
+++ client/src/components/pages/ListingsEdit/SelectWrapped.js
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Typography from "material-ui/Typography";
+import ArrowDropDownIcon from "material-ui-icons/ArrowDropDown";
+import CancelIcon from "material-ui-icons/Cancel";
+import ArrowDropUpIcon from "material-ui-icons/ArrowDropUp";
+import ClearIcon from "material-ui-icons/Clear";
+import Chip from "material-ui/Chip";
+import Select from "react-select";
+import "react-select/dist/react-select.css";
+import Option from "./Option";
+
+function SelectWrapped(props) {
+  const { classes, ...other } = props;
+  const noResultsText = <Typography>{"No results found"}</Typography>;
+  const getArraw = arrowProps => {
+    return arrowProps.isOpen ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />;
+  };
+  const clearIcon = () => <ClearIcon />;
+
+  return (
+    <Select
+      optionComponent={Option}
+      noResultsText={noResultsText}
+      arrowRenderer={getArraw}
+      clearRenderer={clearIcon}
+      valueComponent={valueProps => {
+        const { value, children, onRemove } = valueProps;
+
+        const onDelete = event => {
+          event.preventDefault();
+          event.stopPropagation();
+          onRemove(value);
+        };
+
+        if (onRemove) {
+          return (
+            <Chip
+              tabIndex={-1}
+              label={children}
+              className={classes.chip}
+              deleteIcon={<CancelIcon onTouchEnd={onDelete} />}
+              onDelete={onDelete}
+            />
+          );
+        }
+
+        return <div className="Select-value">{children}</div>;
+      }}
+      {...other}
+    />
+  );
+}
+
+export default SelectWrapped;
