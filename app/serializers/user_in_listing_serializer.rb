2491734ec485c44bd3248c4a5f3f09794f0ef008
Andrei Motinga <andrew.motinga@gmail.com>
2018-05-02 17:37:39 -0400

    remove unused file

--- app/serializers/user_in_listing_serializer.rb
+++
@@ -1,8 +0,0 @@
-class UserInListingSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :avatar_source
-
-  attribute :display_name do |object|
-    object.name || object.email
-  end
-end
