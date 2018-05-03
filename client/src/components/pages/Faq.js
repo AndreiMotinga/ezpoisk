6e31618d2847236a046e62467bd12bc7d617b91c
Andrei Motinga <andrew.motinga@gmail.com>
2018-05-03 14:04:56 -0400

    add faq

--- client/src/components/pages/Faq.js
+++ client/src/components/pages/Faq.js
@@ -3,6 +3,8 @@ import React from "react";
 const Faq = () => (
   <div>
     <h2>Faq</h2>
+    - who built this site? - what is the point of it? - how to contact us? - Is
+    it free?
   </div>
 );
 
