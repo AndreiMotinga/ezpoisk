0ecac1fb20b9f65b2a02d6a5eb73a64c5bb49c97
Andrei Motinga <andrew.motinga@gmail.com>
2018-05-03 10:50:31 -0400

    break home into 2 components

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -2,201 +2,109 @@ import React from "react";
 import Listing from "components/shared/Listing";
 import Grid from "material-ui/Grid";
 import { withStyles } from "material-ui/styles";
-import Api from "api";
-import kinds from "config/kinds";
 import Input from "material-ui/Input";
 import Select from "components/shared/Select";
 import Button from "material-ui/Button";
 import CenteredProgress from "components/shared/CenteredProgress";
 
-class Home extends React.Component {
-  state = {
-    classes: this.props.classes,
-    isLoading: true,
-
-    listings: [],
-    states: [],
-    cities: [],
-    kinds: kinds,
-    timer: null,
-
-    params: {
-      kind: "",
-      state: "",
-      city: "",
-      search: "",
-      page: 1
-    }
-  };
-
-  componentDidMount() {
-    this.fetchListings();
-    this.fetchStates();
-    this.fetchCities();
+const Home = ({
+  isLoading,
+  listings,
+  classes,
+  params,
+  kinds,
+  cities,
+  states,
+  handleChange,
+  handleStateChange,
+  handleSearch,
+  removeListing,
+  loadMore
+}) => {
+  if (isLoading) {
+    return <CenteredProgress />;
   }
 
-  fetchListings = (append = false) => {
-    Api.getListings(this.state.params).then(listings => {
-      let newListings;
-      newListings = append ? this.state.listings.concat(listings) : listings;
-      this.setState({ listings: newListings, isLoading: false });
-    });
-  };
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <div>
+          <Input
+            fullWidth
+            inputComponent={Select}
+            value={params.kind}
+            onChange={handleChange("kind")}
+            placeholder="Раздел"
+            id="kind"
+            inputProps={{
+              name: "kind",
+              instanceId: "kind",
+              simpleValue: true,
+              options: kinds
+            }}
+          />
+          <Input
+            fullWidth
+            inputComponent={Select}
+            value={params.state}
+            onChange={handleStateChange}
+            placeholder="Штат"
+            id="state"
+            inputProps={{
+              name: "state",
+              instanceId: "state",
+              simpleValue: true,
+              options: states
+            }}
+          />
 
-  setStates = states => {
-    this.setState({ states });
-  };
+          <Input
+            fullWidth
+            inputComponent={Select}
+            value={params.city}
+            onChange={handleChange("city")}
+            placeholder="Город"
+            id="city"
+            inputProps={{
+              name: "city",
+              instanceId: "city",
+              simpleValue: true,
+              options: cities
+            }}
+          />
 
-  setCities = cities => {
-    this.setState({ cities });
-  };
-
-  fetchStates = () => {
-    Api.getStates().then(this.setStates);
-  };
-
-  fetchCities = () => {
-    const state = this.state.params.state;
-    Api.getCities(state).then(cities => {
-      this.setState({ cities });
-    });
-  };
-
-  handleChange = name => value => {
-    const params = this.state.params;
-    params[name] = value;
-    this.setState({ params });
-    this.fetchListings();
-  };
-
-  handleStateChange = value => {
-    const params = this.state.params;
-    params.state = value;
-    this.setState({ params });
-    this.fetchCities();
-    this.fetchListings();
-  };
-
-  handleSearch = e => {
-    const params = this.state.params;
-    params.search = e.target.value;
-    this.setState({ params });
-
-    clearTimeout(this.state.timer);
-    const timer = setTimeout(() => {
-      this.fetchListings();
-    }, 1000);
-    this.setState({ timer });
-  };
-
-  removeListing = id => {
-    const listings = this.state.listings.filter(listing => listing.id !== id);
-    this.setState({ listings });
-  };
-
-  loadMore = () => {
-    const params = this.state.params;
-    params.page += 1;
-    this.setState({ params });
-    this.fetchListings(true);
-  };
-
-  render() {
-    const {
-      isLoading,
-      listings,
-      classes,
-      params,
-      kinds,
-      cities,
-      states
-    } = this.state;
-
-    if (isLoading) {
-      return <CenteredProgress />;
-    }
-
-    return (
-      <Grid container>
-        <Grid item xs={12}>
-          <div>
-            <Input
-              fullWidth
-              inputComponent={Select}
-              value={params.kind}
-              onChange={this.handleChange("kind")}
-              placeholder="Раздел"
-              id="kind"
-              inputProps={{
-                name: "kind",
-                instanceId: "kind",
-                simpleValue: true,
-                options: kinds
-              }}
-            />
-            <Input
-              fullWidth
-              inputComponent={Select}
-              value={params.state}
-              onChange={this.handleStateChange}
-              placeholder="Штат"
-              id="state"
-              inputProps={{
-                name: "state",
-                instanceId: "state",
-                simpleValue: true,
-                options: states
-              }}
-            />
-
-            <Input
-              fullWidth
-              inputComponent={Select}
-              value={params.city}
-              onChange={this.handleChange("city")}
-              placeholder="Город"
-              id="city"
-              inputProps={{
-                name: "city",
-                instanceId: "city",
-                simpleValue: true,
-                options: cities
-              }}
-            />
-
-            <Input
-              fullWidth
-              value={params.search}
-              name="search"
-              onChange={this.handleSearch}
-              placeholder="Ключевые слова"
-            />
-          </div>
-        </Grid>
-
-        <Grid item xs={12}>
-          <Grid
-            container
-            spacing={24}
-            direction="column"
-            alignContent="center"
-            justify="center"
-          >
-            {listings.map(listing => (
-              <Grid item key={listing.id} className={classes.listing}>
-                <Listing listing={listing} onRemove={this.removeListing} />
-              </Grid>
-            ))}
-          </Grid>
-        </Grid>
-
-        <Button variant="raised" onClick={this.loadMore}>
-          load more
-        </Button>
+          <Input
+            fullWidth
+            value={params.search}
+            name="search"
+            onChange={handleSearch}
+            placeholder="Ключевые слова"
+          />
+        </div>
       </Grid>
-    );
-  }
-}
+
+      <Grid item xs={12}>
+        <Grid
+          container
+          spacing={24}
+          direction="column"
+          alignContent="center"
+          justify="center"
+        >
+          {listings.map(listing => (
+            <Grid item key={listing.id} className={classes.listing}>
+              <Listing listing={listing} onRemove={removeListing} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+
+      <Button variant="raised" onClick={loadMore}>
+        load more
+      </Button>
+    </Grid>
+  );
+};
 
 const styles = theme => ({
   listing: {
---
+++ client/src/components/pages/Home/HomeContainer.js
@@ -0,0 +1,112 @@
+import React from "react";
+import Api from "api";
+import kinds from "config/kinds";
+import Home from "./Home";
+
+class HomeContainer extends React.Component {
+  state = {
+    classes: this.props.classes,
+    isLoading: true,
+
+    listings: [],
+    states: [],
+    cities: [],
+    kinds: kinds,
+    timer: null,
+
+    params: {
+      kind: "",
+      state: "",
+      city: "",
+      search: "",
+      page: 1
+    }
+  };
+
+  componentDidMount() {
+    this.fetchListings();
+    this.fetchStates();
+    this.fetchCities();
+  }
+
+  fetchListings = (append = false) => {
+    Api.getListings(this.state.params).then(listings => {
+      let newListings;
+      newListings = append ? this.state.listings.concat(listings) : listings;
+      this.setState({ listings: newListings, isLoading: false });
+    });
+  };
+
+  setStates = states => {
+    this.setState({ states });
+  };
+
+  setCities = cities => {
+    this.setState({ cities });
+  };
+
+  fetchStates = () => {
+    Api.getStates().then(this.setStates);
+  };
+
+  fetchCities = () => {
+    const state = this.state.params.state;
+    Api.getCities(state).then(cities => {
+      this.setState({ cities });
+    });
+  };
+
+  handleChange = name => value => {
+    const params = this.state.params;
+    params[name] = value;
+    this.setState({ params });
+    this.fetchListings();
+  };
+
+  handleStateChange = value => {
+    const params = this.state.params;
+    params.state = value;
+    this.setState({ params });
+    this.fetchCities();
+    this.fetchListings();
+  };
+
+  handleSearch = e => {
+    const params = this.state.params;
+    params.search = e.target.value;
+    this.setState({ params });
+
+    clearTimeout(this.state.timer);
+    const timer = setTimeout(() => {
+      this.fetchListings();
+    }, 1000);
+    this.setState({ timer });
+  };
+
+  removeListing = id => {
+    const listings = this.state.listings.filter(listing => listing.id !== id);
+    this.setState({ listings });
+  };
+
+  loadMore = () => {
+    const params = this.state.params;
+    params.page += 1;
+    this.setState({ params });
+    this.fetchListings(true);
+  };
+
+  render() {
+    return (
+      <Home
+        handleChange={this.handleChange}
+        handleStateChange={this.handleStateChange}
+        handleSearch={this.handleSearch}
+        removeListing={this.removeListing}
+        loadMore={this.loadMore}
+        {...this.state}
+      />
+    );
+  }
+}
+
+export default HomeContainer;
