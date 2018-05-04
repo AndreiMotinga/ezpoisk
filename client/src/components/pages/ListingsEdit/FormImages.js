78bfd329cf000f7dc3298cfaaa76dfe9dcc26a50
Andrei Motinga <andrew.motinga@gmail.com>
2018-05-04 19:08:06 -0400

    show loader while loading

--- client/src/components/pages/ListingsEdit/FormImages.js
+++ client/src/components/pages/ListingsEdit/FormImages.js
@@ -5,15 +5,19 @@ import Typography from "material-ui/Typography";
 import Api from "api";
 import IconButton from "material-ui/IconButton";
 import DeleteIcon from "material-ui-icons/Delete";
+import { CircularProgress } from "material-ui/Progress";
 
 class FormImages extends React.Component {
   state = {
+    isLoading: false,
     listing: this.props.listing
   };
 
   handleDrop = acceptedFiles => {
+    this.setState({ isLoading: true });
     const listing = this.state.listing;
-    acceptedFiles.forEach(file => {
+    let uploaded = 0;
+    acceptedFiles.forEach((file, i) => {
       const data = new FormData();
       data.append("picture[listing_id]", listing.id);
       data.append("picture[image]", file);
@@ -22,27 +26,31 @@ class FormImages extends React.Component {
         const pictures = listing.pictures;
         pictures.data.unshift(picture);
         listing.pictures = pictures;
+        uploaded += 1;
         this.setState({ listing });
+
+        if (acceptedFiles.length === uploaded) {
+          this.setState({ isLoading: false });
+        }
       });
     });
   };
 
-  removeImage = res => {
-    const id = res.data.id;
-    const listing = this.state.listing;
-    const data = listing.pictures.data.filter(
-      item => item.id.toString() !== id.toString()
-    );
-    listing.pictures.data = data;
-    this.setState({ listing });
-  };
-
   removePicture = e => {
-    Api.removePicture(e.currentTarget.id).then(this.removeImage);
+    this.setState({ isLoading: true });
+    Api.removePicture(e.currentTarget.id).then(res => {
+      const id = res.data.id;
+      const listing = this.state.listing;
+      const data = listing.pictures.data.filter(
+        item => item.id.toString() !== id.toString()
+      );
+      listing.pictures.data = data;
+      this.setState({ listing, isLoading: false });
+    });
   };
 
   render() {
-    const { listing } = this.state;
+    const { listing, isLoading } = this.state;
     const { classes } = this.props;
 
     return (
@@ -56,25 +64,34 @@ class FormImages extends React.Component {
             нажмите чтобы загрузить
           </Typography>
         </Dropzone>
-        {listing.pictures.data.map(picture => (
-          <div key={picture.id} className={classes.imageContainer}>
-            <img
-              className={classes.image}
-              alt={listing.text}
-              src={picture.attributes.variants.medium}
-            />
-            <div className={classes.actions}>
-              <IconButton
-                className={classes.deleteButton}
-                aria-label="Delete"
-                id={picture.id}
-                onClick={this.removePicture}
-              >
-                <DeleteIcon />
-              </IconButton>
+        <div className={classes.imagesContainer}>
+          {isLoading && (
+            <div className={classes.progressContainer}>
+              <CircularProgress className={classes.progress} />
             </div>
-          </div>
-        ))}
+          )}
+
+          {/* display images */}
+          {listing.pictures.data.map(picture => (
+            <div key={picture.id} className={classes.imageContainer}>
+              <img
+                className={classes.image}
+                alt={listing.text}
+                src={picture.attributes.variants.medium}
+              />
+              <div className={classes.actions}>
+                <IconButton
+                  className={classes.deleteButton}
+                  aria-label="Delete"
+                  id={picture.id}
+                  onClick={this.removePicture}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
@@ -90,6 +107,30 @@ const styles = theme => ({
     borderRadius: 2,
     background: theme.palette.grey[50]
   },
+
+  imagesContainer: {
+    position: "relative"
+  },
+
+  progressContainer: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    zIndex: 1,
+    background: "rgba(250, 250, 250, 0.8)"
+  },
+
+  progress: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    left: 0,
+    bottom: 0,
+    margin: "auto"
+  },
+
   imageContainer: {
     position: "relative",
     display: "inline-block",
