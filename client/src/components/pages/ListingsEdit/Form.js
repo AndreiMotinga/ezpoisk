69ba0781a10ae61e9b1b81041d6bff43be733def
Andrei Motinga <andrew.motinga@gmail.com>
2018-05-04 17:42:14 -0400

    improve form

--- client/src/components/pages/ListingsEdit/Form.js
+++ client/src/components/pages/ListingsEdit/Form.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Grid from "material-ui/Grid";
+import TextField from "material-ui/TextField";
 import Input from "material-ui/Input";
 import Button from "material-ui/Button";
 import Dropzone from "react-dropzone";
@@ -26,110 +28,127 @@ const Form = ({
 
   return (
     <Paper className={classes.paper}>
-      <Dropzone onDrop={handleDrop} />
-      <div>
-        {listing.pictures.data.map(picture => (
-          <img
-            alt={listing.text}
-            key={picture.id}
-            id={picture.id}
-            src={picture.attributes.variants.medium}
-            onClick={removePicture}
-          />
-        ))}
-      </div>
-
       <form onSubmit={handleSubmit}>
-        <Input
-          fullWidth
-          multiline
-          value={listing.text}
-          name="text"
-          onChange={handleTargetChange}
-          placeholder="Текст объявления"
-        />
-
-        <Input
-          fullWidth
-          value={listing.email}
-          name="email"
-          type="email"
-          onChange={handleTargetChange}
-          placeholder="Email"
-        />
-
-        <Input
-          fullWidth
-          type="tel"
-          name="phone"
-          value={listing.phone || ""}
-          onChange={handleTargetChange}
-          placeholder="Phone"
-        />
-
-        <div className={classes.root}>
-          <Input
+        <Grid container spacing={16}>
+          <TextField
             fullWidth
-            inputComponent={Select}
-            value={listing.kind}
-            onChange={handleChange("kind")}
-            placeholder="Раздел"
-            id="kind"
-            inputProps={{
-              name: "kind",
-              instanceId: "kind",
-              simpleValue: true,
-              options: kinds
-            }}
-          />
-          <Input
-            fullWidth
-            inputComponent={Select}
-            value={listing.state}
-            onChange={handleChange("state")}
-            placeholder="Штат"
-            id="state"
-            inputProps={{
-              name: "state",
-              instanceId: "state",
-              simpleValue: true,
-              options: states
-            }}
+            multiline
+            value={listing.text}
+            name="text"
+            onChange={handleTargetChange}
+            label="Текст объявления"
           />
 
-          <Input
-            fullWidth
-            inputComponent={Select}
-            value={listing.city}
-            onChange={handleChange("city")}
-            placeholder="Город"
-            id="city"
-            inputProps={{
-              name: "city",
-              instanceId: "city",
-              simpleValue: true,
-              options: cities
-            }}
-          />
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              value={listing.email}
+              name="email"
+              type="email"
+              onChange={handleTargetChange}
+              label="Email"
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              type="tel"
+              name="phone"
+              value={listing.phone || ""}
+              onChange={handleTargetChange}
+              label="Телефон"
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <Input
+              fullWidth
+              inputComponent={Select}
+              value={listing.kind}
+              onChange={handleChange("kind")}
+              placeholder="Раздел"
+              id="kind"
+              inputProps={{
+                name: "kind",
+                instanceId: "kind",
+                simpleValue: true,
+                options: kinds
+              }}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <Input
+              fullWidth
+              inputComponent={Select}
+              value={listing.state}
+              onChange={handleChange("state")}
+              placeholder="Штат"
+              id="state"
+              inputProps={{
+                name: "state",
+                instanceId: "state",
+                simpleValue: true,
+                options: states
+              }}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <Input
+              fullWidth
+              inputComponent={Select}
+              value={listing.city}
+              onChange={handleChange("city")}
+              placeholder="Город"
+              id="city"
+              inputProps={{
+                name: "city",
+                instanceId: "city",
+                simpleValue: true,
+                options: cities
+              }}
+            />
+          </Grid>
+        </Grid>
+
+        <div className={classes.imagesContainer}>
+          <Dropzone onDrop={handleDrop} />
+          <div className={classes.images}>
+            {listing.pictures.data.map(picture => (
+              <img
+                alt={listing.text}
+                key={picture.id}
+                id={picture.id}
+                src={picture.attributes.variants.medium}
+                onClick={removePicture}
+              />
+            ))}
+          </div>
         </div>
 
-        <Button type="submit" variant="raised">
-          {" "}
-          Сохранить{" "}
-        </Button>
+        <div>
+          <Button type="submit" variant="raised" className={classes.button}>
+            Сохранить
+          </Button>
+        </div>
       </form>
     </Paper>
   );
 };
 
 const styles = {
+  root: {
+    maxWidth: 200
+  },
   paper: {
     maxWidth: 700,
     margin: "20px auto",
     padding: 15
   },
-  root: {
-    maxWidth: 200
+  button: {
+    marginTop: 20
   }
 };
 
