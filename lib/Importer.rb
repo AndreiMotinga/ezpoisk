47faa3d45f324969d728da806fd7f0d715a17fed
Andrei Motinga <andrew.motinga@gmail.com>
2018-05-05 11:04:23 -0400

    update url

--- lib/Importer.rb
+++ lib/Importer.rb
@@ -5,16 +5,13 @@ class Importer
 
   def import
     page = 0
-    url = "https://115561fd.ngrok.io"
+    url = "https://ea95cc46.ngrok.io"
     loop do
       page += 1
       cur_url = url + "?page=#{page}"
       res = JSON(open(cur_url).read)
       res["data"].each do |listing|
-        attrs = listing["attributes"]
-        attrs["created_at"] = attrs["created_at"].to_datetime
-        attrs["updated_at"] = attrs["updated_at"].to_datetime
-        Listing.create(attrs)
+        Listing.create(listing["attributes"])
       end
       p "Processing page #{page} out of #{res['meta']['meta']['total_pages']}"
       break if res["meta"]["meta"]["is_last_page"]
