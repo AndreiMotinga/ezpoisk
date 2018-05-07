663e14b60b7be2946ae51bcc1398e7a340a187d5
Andrei Motinga <andrew.motinga@gmail.com>
2018-05-07 16:37:31 -0400

    add initial ga request

--- client/src/config/history.js
+++ client/src/config/history.js
@@ -2,6 +2,11 @@ import createHistory from "history/createBrowserHistory";
 import ReactGA from "react-ga";
 
 ReactGA.initialize("UA-72628057-1");
+// root url
+if (process.env.NODE_ENV === "production") {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
 
 const history = createHistory();
 
