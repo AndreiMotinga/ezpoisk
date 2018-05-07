105e6215bf5b5099654f2878d273e184da5fdeac
Andrei Motinga <andrew.motinga@gmail.com>
2018-05-07 16:20:21 -0400

    only run in production

--- client/src/config/history.js
+++ client/src/config/history.js
@@ -6,8 +6,10 @@ ReactGA.initialize("UA-72628057-1");
 const history = createHistory();
 
 history.listen((location, action) => {
-  ReactGA.set({ page: location.pathname });
-  ReactGA.pageview(location.pathname);
+  if (process.env.NODE_ENV === "production") {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  }
 });
 
 export default history;
