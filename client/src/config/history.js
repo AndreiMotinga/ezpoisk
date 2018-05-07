34557f555d65a6a6cee02751d93ac49e913485c1
Andrei Motinga <andrew.motinga@gmail.com>
2018-05-07 16:15:50 -0400

    add react-ga

--- client/src/config/history.js
+++ client/src/config/history.js
@@ -1,2 +1,13 @@
 import createHistory from "history/createBrowserHistory";
-export default createHistory();
+import ReactGA from "react-ga";
+
+ReactGA.initialize("UA-72628057-1");
+
+const history = createHistory();
+
+history.listen((location, action) => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
+
+export default history;
