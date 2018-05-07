e1a4e24e11b57f89ba1ebef34ae7bc0a54057b58
Andrei Motinga <andrew.motinga@gmail.com>
2018-05-07 12:51:09 -0400

    handle listing 404

--- client/src/components/pages/ListingsShow.js
+++ client/src/components/pages/ListingsShow.js
@@ -3,22 +3,30 @@ import Api from "api";
 
 import Listing from "components/shared/Listing";
 import CenteredProgress from "components/shared/CenteredProgress";
+import NothingHere from "components/shared/NothingHere";
 
 class ListingsShow extends React.Component {
   state = {
+    isLoading: true,
     listing: null
   };
 
   componentDidMount() {
     const id = this.props.match.params.id;
-    Api.getListing(id).then(res => {
-      this.setState({ listing: res.data.data });
-    });
+    Api.getListing(id)
+      .then(res => {
+        this.setState({ listing: res.data.data, isLoading: false });
+      })
+      .catch(err => {
+        this.setState({ isLoading: false });
+      });
   }
 
   render() {
-    const { listing } = this.state;
-    if (!listing) return <CenteredProgress />;
+    const { listing, isLoading } = this.state;
+    if (isLoading) return <CenteredProgress />;
+
+    if (!listing) return <NothingHere />;
 
     return <Listing listing={listing} />;
   }
